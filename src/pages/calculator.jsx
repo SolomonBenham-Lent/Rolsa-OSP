e00855c2e16da07b32f9670cf01fbd6f47cfb7f4
SolomonBenham-Lent <solomonbenhamlent7576@gmail.com>
2025-12-15 14:17:11 +0000

    CO2 Calculator

--- src/pages/calculator.jsx
+++ src/pages/calculator.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import "../style/calculator.css";
 
 function Input({ label, placeholder, value, setValue }) {
   return (
     <>
-      <label>{label}</label>
+      <br />
+      <label className="calcLabel">{label}</label>
       <input
+        className="calcInput"
         type="number"
         placeholder={placeholder}
         value={value}
@@ -40,9 +43,28 @@ function Calculate({
   const [totalCO2, setTotalCO2] = useState("");
   const [impact, setImpact] = useState("");
 
+  const [showing, setShowing] = useState("none");
+
+  {
+    useEffect(() => {
+      if (impact) {
+        const colorMap = {
+          "Low Impact": "#beefc9ff",
+          "Moderate Impact": "#f8e7b2ff",
+          "High Impact": "tomato",
+        };
+        document.documentElement.style.setProperty(
+          "--impact_colour",
+          colorMap[impact]
+        );
+      }
+    }, [impact]);
+  }
+
   return (
     <>
       <button
+        className="calcButton"
         onClick={() => {
           const m = Number(miles) || 0;
           const e = Number(elec) || 0;
@@ -53,6 +75,8 @@ function Calculate({
           const elecCalc = Number((e * 0.2).toFixed(2));
           const flightsCalc = Number((f * 110).toFixed(2));
           const mealsCalc = Number((mm * 8.1).toFixed(2));
+
+          setShowing("block");
 
           setTotalCO2(
             (milesCalc + elecCalc + flightsCalc + mealsCalc).toFixed(2) + " Kg"
@@ -72,8 +96,13 @@ function Calculate({
       >
         Calculate Total CO2
       </button>
-      <div>{totalCO2}</div>
-      <div>{impact}</div>
+
+      <div id="calcOutput" className="calcOutput" style={{ display: showing }}>
+        <div className="totalCO2">{totalCO2}</div>
+        <div className="impactTable">
+          <div className="calcImpact">{impact}</div>
+        </div>
+      </div>
     </>
   );
 }
@@ -87,45 +116,46 @@ function Calculator() {
   return (
     <>
       <h1>Calculator Page</h1>
-
-      <Input
-        label="Miles Driven Per Month"
-        placeholder={"e.g. 500"}
-        value={miles}
-        setValue={setMiles}
-      />
-      <br />
-      <Input
-        label="Electricity Usage (kWh per month)"
-        placeholder={"e.g. 900"}
-        value={elec}
-        setValue={setElec}
-      />
-      <br />
-      <Input
-        label="Flights Per Month"
-        placeholder={"e.g. 2"}
-        value={flights}
-        setValue={setFlights}
-      />
-      <br />
-      <Input
-        label="Meat-Based Meals Per Month"
-        placeholder={"e.g. 60"}
-        value={meals}
-        setValue={setMeals}
-      />
-      <br />
-      <Calculate
-        miles={miles}
-        setMiles={setMiles}
-        elec={elec}
-        setElec={setElec}
-        flights={flights}
-        setFlights={setFlights}
-        meals={meals}
-        setMeals={setMeals}
-      />
+      <div className="calcContainer">
+        <Input
+          label="Miles Driven Per Month"
+          placeholder={"e.g. 500"}
+          value={miles}
+          setValue={setMiles}
+        />
+        <br />
+        <Input
+          label="Electricity Usage (kWh per month)"
+          placeholder={"e.g. 900"}
+          value={elec}
+          setValue={setElec}
+        />
+        <br />
+        <Input
+          label="Flights Per Month"
+          placeholder={"e.g. 2"}
+          value={flights}
+          setValue={setFlights}
+        />
+        <br />
+        <Input
+          label="Meat-Based Meals Per Month"
+          placeholder={"e.g. 60"}
+          value={meals}
+          setValue={setMeals}
+        />
+        <br />
+        <Calculate
+          miles={miles}
+          setMiles={setMiles}
+          elec={elec}
+          setElec={setElec}
+          flights={flights}
+          setFlights={setFlights}
+          meals={meals}
+          setMeals={setMeals}
+        />
+      </div>
     </>
   );
 }
