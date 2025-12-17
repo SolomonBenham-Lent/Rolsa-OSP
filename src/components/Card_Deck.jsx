b2e92eaf7f6c3cecc7ce0ebe16d00532881e1a59
nhzlei <nhi.le@ada.ac.uk>
2025-12-17 11:33:31 +0000

    imgs for products

--- src/components/Card_Deck.jsx
+++
@@ -1,9 +0,0 @@
-function Card() {
-  return (
-    <>
-      <div>Card deck</div>
-    </>
-  );
-}
-
-export default Card;
---
+++ src/components/Card_Deck/Card_Deck.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Card_Deck.css';
+
+function Card() {
+  return (
+    <>
+      <div className="card_container">
+
+        <div className="card_deck">Card deck</div>
+        
+      </div>
+    </>
+  );
+}
+
+export default Card;
