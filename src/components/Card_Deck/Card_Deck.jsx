a3733ba1efd47a4b56f47cdb85630191d4df49ef
nhzlei <nhi.le@ada.ac.uk>
2025-12-17 12:30:55 +0000

    CARD DECK DONE + RESPONSIVE

--- src/components/Card_Deck/Card_Deck.jsx
+++ src/components/Card_Deck/Card_Deck.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import './Card_Deck.css';
+import solar_panel_green from '../../assets/solar_panel_green.svg';
+import green_car from '../../assets/green_car.svg';
+import green_house from '../../assets/green_house.svg';
+
 
 function Card() {
   return (
     <>
       <div className="card_container">
 
-        <div className="card_deck">Card deck</div>
-        
+        <div className="card_section">
+
+          <div className="top_card_1">
+            <img src={solar_panel_green} alt="solar_panel_green" className="solar_panel_green"/>
+          </div>
+
+          <div className="card_desc">Card deck 1</div>
+
+        </div>
+
+        <div className="card_section">
+          <div className="top_card_2">
+            <img src={green_car} alt="green_car" className="green_car"/>
+          </div>
+          <div className="card_desc">Card deck 2</div>
+        </div>
+
+        <div className="card_section">
+          <div className="top_card_3">
+            <img src={green_house} alt="green_house" className="green_house"/>
+          </div>
+          <div className="card_desc">Card deck 3</div>
+        </div>
+
       </div>
     </>
   );
