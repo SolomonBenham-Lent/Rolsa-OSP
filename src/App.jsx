5b71a352d5547a95677fb9cabeb8251222a4f940
nhzlei <nhi.le@ada.ac.uk>
2025-12-18 10:16:43 +0000

    footer & background responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Home from "./pages/home.jsx"
 import Calculator from './pages/calculator.jsx'
 import Navbar from "./components/Navbar/Navbar.jsx"
 import Footerbar from "./components/Footer/Footerbar.jsx"
+import background_shape_1 from './assets/green_semi_circle.svg'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <img src={background_shape_1} alt="background" className="background_image"/>
       <Footerbar/>
     </div>
   )
