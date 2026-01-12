263029354fcdc070d36ffba21697f433519350c6
nhzlei <nhi.le@ada.ac.uk>
2026-01-12 11:13:38 +0000

    footer fixed bye bye semi circle

--- frontend/src/components/Footer/Footerbar.jsx
+++ frontend/src/components/Footer/Footerbar.jsx
@@ -3,19 +3,14 @@ import { Routes, Route, Link } from "react-router-dom"
 import './Footerbar.css'
 import Copyright from "../../pages/copyright.jsx"
 import Terms from "../../pages/terms.jsx"
-import Accessibility_Terms from "../../pages/accessibilty_terms.jsx"
+import AccessibilityTerms from "../../pages/accessibiltyterms.jsx"
 import Privacy from "../../pages/privacy.jsx"
+import semi_circle from "../../assets/semi_circle.svg"
 
 const Footerbar = () => {
+
   return (
     <>
-    <Routes>
-      <Route path="copyright" element={<Copyright/>}/>
-      <Route path="terms" element={<Terms/>}/>
-      <Route path="accessibility_terms" element={<Accessibility_Terms/>}/>
-      <Route path="privacy" element={<Privacy/>}/>
-    </Routes>
-
     <footer className="footerbar">
         <Link to="copyright">Copyright Notice </Link>
         <Link to="terms">Terms of Service </Link>
@@ -23,6 +18,13 @@ const Footerbar = () => {
         <Link to="privacy">Privacy Policy </Link>
         <div className="footer_text">© 2025 Rolsa Technologies</div>
     </footer>
+
+       <Routes>
+        <Route path="copyright" element={<Copyright/>}/>
+        <Route path="terms" element={<Terms/>}/>
+        <Route path="accessibilityterms" element={<AccessibilityTerms/>}/>
+        <Route path="privacy" element={<Privacy/>}/>
+      </Routes>
     </>
   )
 }
