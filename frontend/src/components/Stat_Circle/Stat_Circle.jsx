fc84642089496144ac8bff6be9934b463dbc6e3e
nhzlei <nhi.le@ada.ac.uk>
2026-01-13 13:56:39 +0000

    make page home page respinsive & card animation

--- frontend/src/components/Stat_Circle/Stat_Circle.jsx
+++ frontend/src/components/Stat_Circle/Stat_Circle.jsx
@@ -5,9 +5,11 @@ import Stat_Circle_svg from "../../assets/stat_circle.svg"
 export default function Stat_Circle({className}){
   return (
     <>
+    <div className="stat_circle_container">
       <div className={`circle_selection ${className || ''}`}>
           <img src={Stat_Circle_svg} alt="stat_circle_svg" className="stat_circle_svg"/>
       </div>
+    </div>
 
 
     </>
